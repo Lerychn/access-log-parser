f8fd022a09d76b513d04723079ebe3e2f63aa3f1
Valery Novikov <lerychn@ya.ru>
2023-06-03 23:14:17 +0300

    checkExist

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int count=0; int x=0;
+        do {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if ((fileExists==false)||(isDirectory==true)) {
+                System.out.println("Файл не существует или указанный путь не является путем к файлу!");
+                continue;
+            }
+            else {
+                count++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер "+count);
+            }
+        }
+        while (x!=5);
     }
 }
