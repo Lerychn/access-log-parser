c04d31787511343785a5be5a10b072024e1f866b
Valery Novikov <lerychn@ya.ru>
2023-06-05 22:46:46 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int count=0; int x=0;
-        do {
+        while (true){
             System.out.println("Введите путь к файлу: ");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
@@ -20,6 +20,5 @@ public class Main {
                 System.out.println("Это файл номер "+count);
             }
         }
-        while (true);
     }
 }
